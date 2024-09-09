c71edf045d5bc6f5b58fdb822567b89c38a4bf56
Diego Perez <112894794+dpzrz@users.noreply.github.com>
2024-09-09 16:28:48 +0100

    Update portscanner.sh

--- Scripts/portscanner.sh
+++ Scripts/portscanner.sh
@@ -1,1 +1,13 @@
 
+#!/bin/bash
+
+hostfile=$1
+portfile=$2
+echo "host,port"
+for host in $(cat $hostfile); do
+	echo "scanning...$host"
+	for port in $(cat $portfile); do
+		echo "scanning...$port"
+	       	timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null && echo "$host,$port"
+  	done
+done
